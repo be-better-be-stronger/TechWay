996829b69f2b3d1cd02d61b20262b29681a8b75b
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2023-04-05 04:48:57 +0700

    hoan thien ProductController

--- src/main/java/com/techway/repository/ProductRepository.java
+++ src/main/java/com/techway/repository/ProductRepository.java
@@ -8,5 +8,5 @@ import com.techway.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	List<Product> findAllByCategoryId(long cid);
+	List<Product> findAllByCategoryId(int cid);
 }
