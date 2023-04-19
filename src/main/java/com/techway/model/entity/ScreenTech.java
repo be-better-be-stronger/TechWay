6e111aa39330b0f30257e21dca08213e6a45ad8a
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2023-04-19 23:24:18 +0700

    update class ScreenTech

--- src/main/java/com/techway/model/entity/ScreenTech.java
+++ src/main/java/com/techway/model/entity/ScreenTech.java
@@ -17,9 +17,11 @@ import org.hibernate.annotations.Nationalized;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter 
+@Setter
 @Entity
 @Table(name = "screenTechs")
 public class ScreenTech implements Serializable {
